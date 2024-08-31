fd47fd459b14ec23554782167693a0855906e710
Tim Hockin <thockin@google.com>
2024-08-31 13:09:53 -0700

    Allow manual trigger of GH actions

--- cmd/myapp-1/main.go
+++ cmd/myapp-1/main.go
@@ -20,6 +20,7 @@ package main
 import (
 	"log"
 
+	_ "github.com/spf13/pflag"
 	"github.com/thockin/go-build-template/pkg/version"
 )
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/thockin/go-build-template
 
 go 1.21
+
+require github.com/spf13/pflag v1.0.5 // indirect
